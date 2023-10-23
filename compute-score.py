5f2d38100eb090b0a3c3375072e80e6b039a28a6
Zak Nafziger <zak.nafziger@amd.com>
2023-10-23 14:43:03 -0700

    Update compute-score.py to include critical-path-wirelength
    also add better table formatting
    
    Signed-off-by: Zak Nafziger <zak.nafziger@amd.com>

--- compute-score.py
+++ compute-score.py
@@ -10,31 +10,60 @@ import re
 
 reWallClockSeconds = re.compile(r'Wall-clock time \(sec\): ([0-9.]+)')
 
-print('%-30s %10s %10s' % ('Benchmark','Wall Clock (sec)', 'Critical-Path Wirelength'))
-print('-----------------------------------------------------------------------')
-
-total = 0
+total_rt = 0
+total_cpw = 0
+results = []
+results.append(('Benchmark', 'Pass', 'Wall Clock (sec)', 'Critical-Path Wirelength'))
 for benchmark in sys.argv[1:]:
-	runtime = float('inf')
-	cpw = float('inf')
-	try:
-		with open(benchmark + '.check') as fp:
-			if fp.readline().rstrip() != 'PASS':
-				continue
-		with open(benchmark + '.phys.log') as fp:
-			last = fp.readlines()[-1].rstrip()
-		m = reWallClockSeconds.match(last)
-		if not m:
-			continue
-		runtime = m.group(1)
-		with open(benchmark + '.wirelength') as fp:
-			lines = fp.readlines()
-			for l in lines:
-				if 'Wirelength: ' in l:
-					cpw = l.split()[-1]
-					break
-	finally:
-		total += float(runtime)
-		print('%-30s %10s %10s' % (benchmark,runtime,cpw))
-print('-----------------------------------------------------------------------')
-print('%-30s %10.2f' % ('Total',total))
+    runtime = float('inf')
+    cpw = float('inf')
+    check = True
+    try:
+        with open(benchmark + '.check') as fp:
+            if fp.readline().rstrip() != 'PASS':
+                check = False
+        with open(benchmark + '.phys.log') as fp:
+            last = fp.readlines()[-1].rstrip()
+        m = reWallClockSeconds.match(last)
+        if not m:
+            continue
+        runtime = float(m.group(1))
+        with open(benchmark + '.wirelength') as fp:
+            lines = fp.readlines()
+            for l in lines:
+                if 'Wirelength: ' in l:
+                    cpw = int(l.split()[-1])
+                    break
+    finally:
+        results.append((benchmark,check,runtime,cpw))
+        total_rt += runtime
+        total_cpw += cpw
+
+results.append(('Total', '', '{:.2f}'.format(total_rt), total_cpw))
+
+column_widths = [0]*len(results[0])
+for row in results:
+    for index, item in enumerate(row):
+        if len(str(item)) > column_widths[index]:
+            column_widths[index] = len(str(item))
+
+sep = '|'
+header_format_str = sep
+body_format_str = sep
+horiz = '+'
+for index, width in enumerate(column_widths):
+    header_format_str += ' %-' + str(width) + 's ' + sep
+    horiz += '-' + '-'*width + '-' + '+'
+    if index == 0:
+        body_format_str += ' %-' + str(width) + 's ' + sep
+    else:
+        body_format_str += ' %' + str(width) + 's ' + sep
+
+print(horiz)
+print(header_format_str % results[0])
+print(horiz)
+for row in results[1:-1]:
+    print(body_format_str % row)
+print(horiz)
+print(body_format_str % results[-1])
+print(horiz)
