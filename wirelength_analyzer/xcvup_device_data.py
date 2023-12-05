f1816b68c8e9972e37191b0f469315ee05c9a85a
Eddie Hung <eddie.hung@amd.com>
2023-12-05 12:20:42 -0800

    OUTBUF -> OBUFT
    Signed-off-by: Eddie Hung <eddie.hung@amd.com>

--- wirelength_analyzer/xcvup_device_data.py
+++ wirelength_analyzer/xcvup_device_data.py
@@ -80,7 +80,7 @@ class xcvupDeviceData:
 
             'IBUFCTRL':        self.all_to_all,
             'INBUF':           self.all_to_all,
-            'OUTBUF':          self.all_to_all,
+            'OBUFT':           self.all_to_all,
 
             # The following cell types are BELs that make up a DSP macro.
             # Such DSPs contains a number of optional pipelining registers,
